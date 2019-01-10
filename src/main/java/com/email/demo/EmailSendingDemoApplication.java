0195e5677d7fe6f6d39099f1576dbee996d94225
akd333 <kumarashishdhar@gmail.com>
2019-01-10 15:34:06 +0530

    first commit

---
+++ src/main/java/com/email/demo/EmailSendingDemoApplication.java
@@ -0,0 +1,14 @@
+package com.email.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendingDemoApplication.class, args);
+	}
+
+}
+
